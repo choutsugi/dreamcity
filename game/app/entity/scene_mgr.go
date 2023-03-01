4caf00f8f864183c665452771d10239f13aa0e07
Kevin <kevin.zhang@deeperse.com>
2023-03-01 14:58:44 +0800

    场景管理

---
+++ game/app/entity/scene_mgr.go
@@ -0,0 +1,26 @@
+package entity
+
+import "github.com/dobyte/due/errors"
+
+// SceneMgr 场景管理器
+type SceneMgr struct {
+	scenes map[string]*Scene
+}
+
+func NewSceneMgr(opts []*SceneOptions) *SceneMgr {
+	mgr := &SceneMgr{scenes: make(map[string]*Scene, len(opts))}
+	for i := range opts {
+		scene := newScene(opts[i])
+		mgr.scenes[scene.ID] = scene
+	}
+	return mgr
+}
+
+// GetScene 获取场景
+func (mgr *SceneMgr) GetScene(sceneID string) (*Scene, error) {
+	scene, ok := mgr.scenes[sceneID]
+	if !ok {
+		return nil, errors.New("场景不存在")
+	}
+	return scene, nil
+}
---
+++ game/app/entity/scene_options.go
@@ -0,0 +1,12 @@
+package entity
+
+type SceneOptions struct {
+	ID     string `msgpack:"id"`
+	Name   string `msgpack:"name"`
+	MinX   int    `msgpack:"minX"`
+	MaxX   int    `msgpack:"maxX"`
+	ContsX int    `msgpack:"contsX"`
+	MinY   int    `msgpack:"minY"`
+	MaxY   int    `msgpack:"maxY"`
+	ContsY int    `msgpack:"contsY"`
+}
